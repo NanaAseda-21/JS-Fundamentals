8da1cb99a76e3099e1a2fe83ee8c54ca9cec4da5
Jeddy Kendall Hammond <jeddyhammond@gmail.com>
2025-07-14 21:35:27 +0000

    Add files via upload

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const size = parseInt(process.argv[2]);
 
-if (!isNaN(size) && size > 0) {
+if (!isNaN(size) && Number.isInteger(size) && size > 0) {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
   }
